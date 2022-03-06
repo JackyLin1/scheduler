8fd8a695574f61778efb3466f148570ce74a8e38
JackyLin1 <jlin9702@gmail.com>
2022-03-06 10:31:16 +0900

    Update: shows spots remainning

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,22 +10,36 @@ export default function useApplicationData () {
     interviewers: {},
   });
 
+  const updateSpots = (num) => { 
+    let filteredDay = state.days.filter(d => d.name === state.day)[0];
+    const Days = [...state.days];
+    Days.forEach (d => {
+      if (d.id === filteredDay.id) {
+        d.spots += num;
+      };
+    });
+ 
+    return Days;
+  }
+
   function bookInterview (id, interview) {
     const appointment = {...state.appointments[id], interview: { ...interview }};
     const appointments = {...state.appointments,[id]: appointment};
+    const spots = updateSpots(-1);
      return axios.put(`/api/appointments/${id}`, { interview })
     .then(
-      setState({ ...state, appointments})
+      setState({ ...state, appointments, spots})
     );
   };
 
   function onDelete(id) {
     const appointment = { ...state.appointments[id], interview: null };
     const appointments = { ...state.appointments, [id]: appointment };
+    const spots = updateSpots(+1);
     return axios.delete(`http://localhost:8001/api/appointments/${id}`, appointment)
       .then( 
-        setState({...state, appointments
-      }));
+        setState({...state, appointments, spots})
+      );
   };
 
   const setDay = day => setState({...state, day})
